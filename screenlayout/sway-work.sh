f6d36a3707e4f371f8112b04c2d35f3b999d14fa
Tancredi Orlando <tancredi.orlando@gmail.com>
2017-12-01 10:38:41 +0100

    Put internal screen in the middle

--- screenlayout/sway-work.sh
+++ screenlayout/sway-work.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-swaymsg output LVDS-1   pos 320 1080 res 1600x900
+swaymsg output LVDS-1   pos 160 1080 res 1600x900
 swaymsg output HDMI-A-2 pos 0   0    res 1920x1080
--- screenlayout/x11-work.sh
+++ screenlayout/x11-work.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr                                                                          \
-    --output LVDS1 --primary --mode 1600x900 --pos 320x1080 --rotate normal     \
+    --output LVDS1 --primary --mode 1600x900 --pos 160x1080 --rotate normal     \
     --output VGA1 --off                                                         \
     --output HDMI1 --off                                                        \
     --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --set "audio" on  \
