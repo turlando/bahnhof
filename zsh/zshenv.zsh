51ad7133ad495858a3e8087cc448ed09d55b6633
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-08-08 08:27:42 +0200

    Refactor zsh config

---
+++ zsh/zshenv.zsh
@@ -0,0 +1,15 @@
+## PATH
+export PATH="$PATH:$HOME/.local/bin"
+
+## Useful environment variables
+export PAGER='less'
+export EDITOR='nvim'
+export BROWSER='firefox'
+
+## ssh-agent
+eval $(systemctl --user show-environment | grep SSH_AUTH_SOCK)
+export SSH_AUTH_SOCK
+
+## Sway
+export XKB_DEFAULT_LAYOUT='it'
+export SWAY_CURSOR_THEME='Neutral'
