593675b86ba0313c388da6faeecda5f39a850cc6
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-08-25 16:52:21 +0200

    Add mpctl script

---
+++ bin/mpctl.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -eu
+
+##
+## NAME
+##        mpctl - Control MPRIS-enabled players
+##
+## SYNOPSIS
+##        mpctl OPTIONS
+##
+## DESCRIPTION
+##        Issue commands to the default MPRIS player.
+##
+##        Available options:
+##
+##        p|play|pause
+##               Pause or resume playback
+##
+##        n|next
+##               Skip to next track
+##
+##        b|prev|previous
+##               Skip to previous track
+##
+
+
+# Die if no arguments are provided.
+if [ $# -eq 0 ]; then
+    exit 1
+fi
+
+case "${1:-}" in
+    p|play|pause)
+        COMMAND="PlayPause"
+        ;;
+    n|next)
+        COMMAND="Next"
+        ;;
+    b|prev|previous)
+        COMMAND="Previous"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+dbus-send                                      \
+    --session                                  \
+    --print-reply                              \
+    --dest=org.mpris.MediaPlayer2.quodlibet    \
+    /org/mpris/MediaPlayer2                    \
+    "org.mpris.MediaPlayer2.Player.${COMMAND}" \
+    &>/dev/null
+
+exit 0
