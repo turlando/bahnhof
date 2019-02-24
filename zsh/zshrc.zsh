8353e6c4003f43281be6b216857e892a7c2a7689
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-02-24 18:29:16 +0100

    Add alias to download webpage with wget

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -2,3 +2,18 @@ setopt vi
 
 alias vi='nvim'
 alias vim='nvim'
+
+## Download entire webpage.
+alias wdump='wget \
+                --timestamping \
+                --page-requisites \
+                --no-parent \
+                --adjust-extension \
+                --convert-links \
+                --no-cookies \
+                --restrict-file-names=windows \
+                --no-directories \
+                -e robots=off'
+
+## Download entire webpage including external files.
+alias wdumpr='wdump --span-hosts'
