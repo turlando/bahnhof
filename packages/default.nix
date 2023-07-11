564bc63f00edcb2194cc77238c97daacc41150ce
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-07-11 21:50:20 +0200

    s

--- packages/default.nix
+++ packages/default.nix
@@ -2,6 +2,6 @@
 
 {
   lsws = pkgs.callPackage ./lsws {};
-  menu = pkgs.callPackage (import ./menu).menu {};
-  menu-run = pkgs.callPackage (import ./menu).menu-run {};
+  menu = pkgs.callPackage (import ./menu { pkgs = pkgs }).menu {};
+  menu-run = pkgs.callPackage (import ./menu { pkgs = pkgs }).menu-run {};
 }
